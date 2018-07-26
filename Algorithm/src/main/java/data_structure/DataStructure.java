c90506b4ff9141f04ca2cc7d58538522891357c7
jingbolee <jingbolee@foxmail.com>
2018-07-26 14:49:52 +0800

    string 转 int

--- Algorithm/src/main/java/data_structure/DataStructure.java
+++ Algorithm/src/main/java/data_structure/DataStructure.java
@@ -5,6 +5,25 @@ import algorithms.Node;
 public class DataStructure {
 
     /**
+     * string 转 int
+     * @param input
+     * @return
+     */
+    public static int string2Int(String input) {
+        int value = 0;
+        if (input == null) {
+            value = 0;
+        } else {
+            char[] chars = input.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                value = value * 10 + (chars[i] - '0');
+            }
+        }
+        return value;
+    }
+
+
+    /**
      * 删除单列表中重复元素
      *
      * @param head
@@ -29,7 +48,7 @@ public class DataStructure {
     public static void printNode(Node head) {
         if (head != null) {
             StringBuilder sb = new StringBuilder();
-            sb.append("Node concent: " );
+            sb.append("Node concent: ");
             Node temp = head;
             while (temp != null) {
                 sb.append("->" + temp.data);
@@ -42,14 +61,17 @@ public class DataStructure {
     }
 
     public static void main(String... args) {
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(3);
-        head.next.next.next.next = new Node(3);
-        head.next.next.next.next.next = new Node(3);
-        Node node = deleteDuplicatesNodeFromLinkedlist(head);
-        printNode(node);
+//        Node head = new Node(1);
+//        head.next = new Node(2);
+//        head.next.next = new Node(3);
+//        head.next.next.next = new Node(3);
+//        head.next.next.next.next = new Node(3);
+//        head.next.next.next.next.next = new Node(3);
+//        Node node = deleteDuplicatesNodeFromLinkedlist(head);
+//        printNode(node);
+
+        int anInt = string2Int("12345");
+        System.out.println("" + anInt);
     }
 
 }
